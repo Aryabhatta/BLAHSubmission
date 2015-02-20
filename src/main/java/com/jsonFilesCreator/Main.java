6a628db7d6f7a5b5b1d91c44f5a00d5f775eb2b8
shrikant <shrikantpvinchurkar@gmail.com>
2015-02-20 20:07:47 +0100

    - Adding option for different output types - change in the way, identifier urls are generated for multiple normalized ids

--- src/main/java/com/jsonFilesCreator/Main.java
+++ src/main/java/com/jsonFilesCreator/Main.java
@@ -23,15 +23,19 @@ import org.json.simple.JSONObject;
 public class Main {
 
 	public static void main(String[] args) {
-/**/		
-		if(args.length < 4) {
+/**/	
+		if(args.length < 5) {
 			System.out.println("Usage: ");
-			System.out.println("java -jar jsonFilesCreator-1.0.one-jar.jar [pathToHTMLfiles] [pathToJSONfiles] [intermediateFilePath] [outputDirectory] [concatString]\n");
+			System.out.println("java -jar jsonFilesCreator-1.0.one-jar.jar [pathToHTMLfiles] [pathToJSONfiles] [intermediateFilePath] [outputDirectory] [outputType] [concatString]\n");
 			System.out.println("1. [pathToHTMLfiles]-Mandatory: Path to folder containing html files");
 			System.out.println("2. [pathToJSONfiles]-Mandatory: Path to folder containing json files");
 			System.out.println("3. [intermediateFilePath]-Mandatory: Path to interFile.tsv");
 			System.out.println("4. [outputDirectory]-Mandatory: Path to folder where final json files would be written");
-			System.out.println("5. [concatString]-Optional: string used for concatenating title and abstract. By default space is used for concat \n\n");
+			System.out.println("5. [outputType]-Mandatory: Type of files to be created");
+			System.out.println("\t if outputType=1, separate json files for title and abstract");
+			System.out.println("\t if outputType=2, combined file by concatenating title and abstract");
+			System.out.println("\t if outputType=3, all possible files (separate and combined) are produced");
+			System.out.println("6. [concatString]-Optional: string used for concatenating title and abstract. By default space is used for concatenation \n\n");
 			return;
 		}
 		String titleAbsConcatStr = " ";
@@ -52,9 +56,11 @@ public class Main {
 		if(outputDirectory.charAt(outputDirectory.length()-1)!='/') {
 			outputDirectory += "/";
 		}
+		
+		int outputType = Integer.parseInt(args[4]);
 
-		if(args.length==5) {
-			titleAbsConcatStr = args[4];
+		if(args.length==6) {
+			titleAbsConcatStr = args[5];
 		}
 /**/		
 
@@ -64,6 +70,7 @@ public class Main {
 		String jsonDirectory = "/home/shrikant/NLPRE/shrikant-master-thesis/LocTextCorpus/annotations/";
 		String intermediateFilePath = "workDir/interFile.tsv";
 		String outputDirectory = "workDir/jsonFiles/";
+		int outputType=3; 
 */
 
 		boolean allowPORelations = false;
@@ -86,8 +93,186 @@ public class Main {
 			String jsonFilePath = jsonDirectory + pubmedId + ".json";
 			JsonReader reader = new JsonReader(jsonFilePath);			
 
-			writeJsonFile(pubmedId, docList, docIndex,parser,reader,outputDirectory, titleAbsConcatStr, allowPORelations);
+			if(outputType==1) {
+				writeTitleJsonFile(pubmedId, docList, docIndex,parser,reader,outputDirectory, allowPORelations);
+				writeAbstractJsonFile(pubmedId, docList, docIndex,parser,reader,outputDirectory, titleAbsConcatStr, allowPORelations);
+			} else if(outputType==2) {
+				writeJsonFile(pubmedId, docList, docIndex,parser,reader,outputDirectory, titleAbsConcatStr, allowPORelations);	
+			} else if(outputType==3) {
+				writeTitleJsonFile(pubmedId, docList, docIndex,parser,reader,outputDirectory, allowPORelations);
+				writeAbstractJsonFile(pubmedId, docList, docIndex,parser,reader,outputDirectory, titleAbsConcatStr, allowPORelations);
+				writeJsonFile(pubmedId, docList, docIndex,parser,reader,outputDirectory, titleAbsConcatStr, allowPORelations);	
+			} else {
+				System.err.println("Invalid output type. See Usage. Valid options are 1,2 or 3");
+			}
 		}
+	}
+
+	private static void writeTitleJsonFile(String pubmedId,
+			ArrayList<Document> docList, HashMap<String, Integer> docIndex,
+			htmlParser parser, JsonReader reader, String outputDirectory,
+			boolean allowPORelations) {
+
+		JSONObject jsonObject = new JSONObject();
+
+		String titleText = parser.getTitle(); 
+
+		jsonObject.put("text", titleText);
+		jsonObject.put("sourcedb", "PubMed");
+		jsonObject.put("sourceid",pubmedId);
+
+		int entCounter = 1;
+
+		HashMap<Integer,String> idMap = new HashMap<Integer,String>();
+
+		JSONArray denotationArr = new JSONArray();
+		for(Entity ent:reader.getTitleEntities()){
+			JSONObject entObj = new JSONObject();
+			String identifier = "T" + entCounter++;
+			entObj.put("id", identifier);
+
+			JSONObject spanObj = new JSONObject();
+			int start = ent.getStart();
+			int end = ent.getEnd();
+
+			idMap.put(start, identifier);
+
+			spanObj.put("begin", start);
+			spanObj.put("end", end );
+
+			entObj.put("span", spanObj);
+
+			String normalizedId = searchNormalizedId(pubmedId, docList, docIndex, "title", start, end);
+			entObj.put("obj", normalizedId);
+			denotationArr.add(entObj);
+		}
+		
+		jsonObject.put("denotations", denotationArr);
+
+		// Write Relations Now
+		int relCounter=1;
+		JSONArray relationArr = new JSONArray();
+
+		for(Relation rel:reader.getTitleRelations()){
+			Entity protEntity = rel.getProteinEntity();
+			Entity nonProtEntity = rel.getNonProteinEntity();
+
+			if(!allowPORelations && 
+					nonProtEntity.getType()==EntityType.Organism){
+				continue;
+			}
+
+			JSONObject relObj = new JSONObject();
+			String identifier = "R" + relCounter++;
+
+			relObj.put("id", identifier);
+
+			int entStart = protEntity.getStart();
+			relObj.put("subj", idMap.get(entStart));
+
+			if(nonProtEntity.getType()==EntityType.Location){
+				relObj.put("pred", "localizeTo");
+			} else {
+				relObj.put("pred", "belongsTo");
+			}
+
+			relObj.put("obj", idMap.get(nonProtEntity.getStart()));
+			relationArr.add(relObj);
+		}
+		
+		jsonObject.put("relations", relationArr);
+
+		String outputFilePath = outputDirectory + pubmedId + "-title.json";
+		try {
+			FileWriter fw = new FileWriter(outputFilePath);
+			fw.write(jsonObject.toJSONString());
+			fw.close();
+		} catch (IOException e) {
+			System.err.println("Error writing file: " + outputFilePath);
+		}
+	}
+
+	private static void writeAbstractJsonFile(String pubmedId,
+			ArrayList<Document> docList, HashMap<String, Integer> docIndex,
+			htmlParser parser, JsonReader reader, String outputDirectory,
+			String titleAbsConcatStr, boolean allowPORelations) {
+
+		JSONObject jsonObject = new JSONObject();
+
+		String abstractText = parser.getAbstract();
+
+		jsonObject.put("text", abstractText);
+		jsonObject.put("sourcedb", "PubMed");
+		jsonObject.put("sourceid",pubmedId);
+
+		int entCounter = 1;
+
+		HashMap<Integer,String> idMap = new HashMap<Integer,String>();
+
+		JSONArray denotationArr = new JSONArray();
+		for(Entity ent:reader.getAbsEntities()){
+			JSONObject entObj = new JSONObject();
+			String identifier = "T" + entCounter++;
+			entObj.put("id", identifier);
+
+			JSONObject spanObj = new JSONObject();
+			int start = ent.getStart();
+			int end = ent.getEnd();
+
+			idMap.put(start, identifier);
+
+			spanObj.put("begin", start);
+			spanObj.put("end", end );
+
+			entObj.put("span", spanObj);
+
+			String normalizedId = searchNormalizedId(pubmedId, docList, docIndex, "abs", ent.getStart(), ent.getEnd());
+			entObj.put("obj", normalizedId);
+			denotationArr.add(entObj);
+		}
+		jsonObject.put("denotations", denotationArr);
+
+		// Write Relations Now
+		int relCounter=1;
+		JSONArray relationArr = new JSONArray();
+		for(Relation rel:reader.getAbsRelations()){
+			Entity protEntity = rel.getProteinEntity();
+			Entity nonProtEntity = rel.getNonProteinEntity();
+
+			if(!allowPORelations && 
+					nonProtEntity.getType()==EntityType.Organism){
+				continue;
+			}
+
+			JSONObject relObj = new JSONObject();
+			String identifier = "R" + relCounter++;
+
+			relObj.put("id", identifier);
+
+			int protEntStart = protEntity.getStart();
+			relObj.put("subj", idMap.get(protEntStart));
+
+			if(nonProtEntity.getType()==EntityType.Location){
+				relObj.put("pred", "localizeTo");
+			} else {
+				relObj.put("pred", "belongsTo");
+			}
+
+			int nonProtEntStart = nonProtEntity.getStart();					
+			relObj.put("obj", idMap.get(nonProtEntStart));
+			relationArr.add(relObj);
+		}
+		jsonObject.put("relations", relationArr);
+
+		String outputFilePath = outputDirectory + pubmedId + "-abstract.json";
+		try {
+			FileWriter fw = new FileWriter(outputFilePath);
+			fw.write(jsonObject.toJSONString());
+			fw.close();
+		} catch (IOException e) {
+			System.err.println("Error writing file: " + outputFilePath);
+		}
+		
 	}
 
 	private static void writeJsonFile(String pubmedId,
@@ -238,7 +423,10 @@ public class Main {
 		for(Entity ent:entList){
 			if(ent.getStart()==start && ent.getEnd()==end){
 				retString = ent.getNormalizedIdentifier();
-				if(retString.equals("TODO") || retString.contains(",")){
+				if(retString.equals("TODO") ||
+						retString.toLowerCase().contains("protein") ||						
+						retString.toLowerCase().contains("organism") ||
+						retString.toLowerCase().contains("location")){
 					if(ent.getType()==EntityType.Protein){
 						retString = "Protein";
 					} else if(ent.getType()==EntityType.Location){
@@ -248,7 +436,19 @@ public class Main {
 					}
 				} else {
 					if(ent.getType()==EntityType.Protein){
-						retString = "http://identifiers.org/uniprot/" + retString;
+						if(retString.contains(",")) {
+							String tokens[] = retString.split(",");
+							
+							String concatenatedUrls = "";
+							for(int i=0; i<tokens.length; i++){
+								if(!concatenatedUrls.isEmpty()) {
+									concatenatedUrls += ",";
+								}
+								concatenatedUrls += "http://identifiers.org/uniprot/" + tokens[i];
+							}
+						} else {
+							retString = "http://identifiers.org/uniprot/" + retString;	
+						}
 					} else if(ent.getType()==EntityType.Location){
 						String tokens[] = retString.split("\\s+");
 						if(tokens[0].substring(0,3).equals("GO:")) {
@@ -288,7 +488,6 @@ public class Main {
 		try {
 			while((line=br.readLine())!=null){
 				String tokens[] = line.split("\\t+");
-
 				String docId = tokens[0];
 
 				Document currentDoc = null;
@@ -311,9 +510,9 @@ public class Main {
 					ent.setType(EntityType.Protein);
 				}
 
-				ent.setNormalizedIdentifier(tokens[3]);
-				ent.setStart(Integer.parseInt(tokens[5]));
-				ent.setEnd(Integer.parseInt(tokens[6]));
+				ent.setNormalizedIdentifier(tokens[3].trim());
+				ent.setStart(Integer.parseInt(tokens[5].trim()));
+				ent.setEnd(Integer.parseInt(tokens[6].trim()));
 
 				if(tokens[4].equals("title")) {
 					ent.setPart(EntityPart.title);
